d026a4249521d6cdbd17b465a55579a64b8c2f4a
Steve Canneaux <seamlessloops@gmail.com>
2022-02-15 19:43:04 +0000

    Add files via upload

---
+++ dictionaries Lesson 1.py
@@ -0,0 +1,32 @@
+ddd= dict()
+ddd["age"] = 21
+ddd["course"] = 182
+print(ddd)
+
+ddd["age"] = 23
+print(ddd)
+
+ccc= dict()
+ccc["dave"] = 1
+ccc["jane"]= 1
+
+print(ccc)
+ccc["jane"] = ccc["jane"] +1
+print(ccc)
+
+purse = dict()
+purse['money'] = 12
+purse['sweets'] = 3
+purse['tissues'] = 75
+
+print(purse)
+
+print(purse['sweets'])
+
+purse['sweets'] = purse['sweets']
+
+jjj = {'dave' : 1, 'fred' : 42, 'jane': 100}
+print(jjj)
+
+ooo = {}
+print(ooo)
